93a47f1e9ca79249dfe8208322b7308bda85aa69
Prateek kumar rai <54741353+prateekrai123@users.noreply.github.com>
2021-12-23 09:21:16 +0530

    Update ComingSoon.js

--- ComingSoon.js
+++ ComingSoon.js
@@ -1,21 +1,42 @@
-var navbarMenu = document.querySelector(".sidemenu");
-function navbarMenutoggle(){
-    navbarMenu.classList.toggle("sidemenuheight");
+const emailAddBtn = document.getElementById("btn");
 
+async function onEmailAddBtnClick() {
+  const value = document.getElementById("email").value;
+  const params = {
+    method: "post",
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+    body: JSON.stringify({ email: value }),
+  };
+
+  await fetch("https://techfest2022sliet.herokuapp.com/storeEmail", params)
+    .then((res) => {
+      if (res.status == 200) {
+        alert(
+          "Thank you for subscribing to Techfest. We will send you updates on your email"
+        );
+      } else {
+        alert("Some error occurred. Please try again");
+      }
+    })
+    .catch((err) => {
+      alert("Some error occurred. Please try again");
+    });
 }
 
-
+var navbarMenu = document.querySelector(".sidemenu");
+function navbarMenutoggle() {
+  navbarMenu.classList.toggle("sidemenuheight");
+}
 
 // gallery part code is herer...............
 
 // manue code herer..........
 
-
 function openNav() {
-    document.getElementById("mySidenav").style.width = "200px";
-    document.getElementById("mainMenu").style.marginright = "200px";
-  }
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("mainMenu").style.marginleft = "0";
-  }
+  document.getElementById("mySidenav").style.width = "200px";
+  document.getElementById("mainMenu").style.marginright = "200px";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mainMenu").style.marginleft = "0";
+}
